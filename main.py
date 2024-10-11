cc86b5b563b02fed3ef9c6944c9fcdf7d3bd1e58
krisMG21 <cristimg9@gmail.com>
2024-10-11 12:31:21 +0200

    Formatter :/

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import argparse
 
 config = json.load(open("config.json"))
 
+
 def prepare_data(csv_file):
     # Load and preprocess data
     raw_data = pd.read_csv(csv_file)
@@ -22,6 +23,7 @@ def prepare_data(csv_file):
 
     return n, m, X, Y
 
+
 def test_config(Y):
     # Check if the number of inputs and outputs match the configuration
     in_size = config["input_size"]
@@ -34,6 +36,7 @@ def test_config(Y):
         print("Got: ", in_size, [x_max, x_min])
         exit()
 
+
 def main(csv_file, mode):
     n, m, X, Y = prepare_data(csv_file)
 
@@ -43,27 +46,27 @@ def main(csv_file, mode):
     W1, b1, W2, b2 = np.array(""), np.array(""), np.array(""), np.array("")
 
     # Train or test
-    if mode == 'train':
+    if mode == "train":
         print("Training...")
         iterations = config["iterations"]
         W1, b1, W2, b2 = nn.gradient_descent(X, Y, 0.10, iterations, m)
-    elif mode == 'test':
+    elif mode == "test":
         W1, b1, W2, b2 = nn.init_params()
 
         # See 20 first (if possible) predictions
         if config["show_first"] > 0:
-            print("="*100 + "\nFirst predictions:\n" + "="*100)
+            print("=" * 100 + "\nFirst predictions:\n" + "=" * 100)
             for i in range(min(config["show_first"], n)):
                 nn.test_prediction(i, W1, b1, W2, b2, X, Y)
 
         # Test fails
         if config["show_fails"]:
-            print("="*100 + "\nFails:\n" + "="*100)
+            print("=" * 100 + "\nFails:\n" + "=" * 100)
             nn.show_fails(W1, b1, W2, b2, X, Y)
 
         # Test accuracy
         if config["show_accuracy"]:
-            print("="*100 + "\nAccuracy:\n" + "="*100)
+            print("=" * 100 + "\nAccuracy:\n" + "=" * 100)
             _, _, _, A2 = nn.forward_prop(W1, b1, W2, b2, X)
             print(nn.get_accuracy(nn.get_predictions(A2), Y))
 
@@ -72,21 +75,22 @@ def main(csv_file, mode):
         exit()
 
     # Serializing of network status
-    data = {
-        "W1": W1.tolist(),
-        "b1": b1.tolist(),
-        "W2": W2.tolist(),
-        "b2": b2.tolist()
-    }
+    data = {"W1": W1.tolist(), "b1": b1.tolist(), "W2": W2.tolist(), "b2": b2.tolist()}
 
     # Serialize weights
     json_data = json.dumps(data, indent=2)
     with open("nn_weights.json", "w") as json_file:
         json_file.write(json_data)
 
-if (__name__ == "__main__"):
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("csv_file", help="Path to the CSV file for training/testing")
-    parser.add_argument("--mode", choices=['train', 'test'], required=True, help="Mode: 'train' or 'test'")
+    parser.add_argument(
+        "--mode",
+        choices=["train", "test"],
+        required=True,
+        help="Mode: 'train' or 'test'",
+    )
     args = parser.parse_args()
     main(args.csv_file, args.mode)
